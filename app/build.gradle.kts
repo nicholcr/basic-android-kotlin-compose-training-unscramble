5119bbb881139197f4fc25440500030ad4fe2163
nicholcr <nicholcr@umich.edu>
2025-05-05 15:04:18 -0500

    add unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,7 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    testImplementation("junit:junit:4.13.2")
 }
